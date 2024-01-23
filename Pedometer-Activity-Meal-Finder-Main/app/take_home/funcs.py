d07c75e84bb3ec7fcfee94417bbc51eca4e5311f
Daniel Smith <dev.danieljosiahsmith.com>
2024-01-23 16:35:07 +0800

    add number of meal options, fix a few bugs

--- Pedometer-Activity-Meal-Finder-Main/app/take_home/funcs.py
+++ Pedometer-Activity-Meal-Finder-Main/app/take_home/funcs.py
@@ -3,15 +3,15 @@ import requests
 
 
 
-def get_dinner_options(steps):
+def get_dinner_options(steps,num_of_meals):
     #API VARIABLES
     api_key = settings.SPOONACULAR_API_KEY
     base_url = settings.SPOONACULAR_BASE_URL
 
     #Calculate min and max calorie range for meal options
-    min_calories, max_calories = calc_calories(steps)
+    min_calories, max_calories = calc_calories(steps,num_of_meals)
 
-    filter = f'?&minCalories={min_calories}&maxCaloreis=1000&number=2&random=true'
+    filter = f'?&minCalories={min_calories}&maxCalories={max_calories}&number=2&random=true'
 
     #Build url
     url = f'{base_url}{filter}&apiKey={api_key}'
@@ -19,15 +19,23 @@ def get_dinner_options(steps):
     #GET request from Spoonacular API
     resp = requests.get(url,timeout=10)
 
-    data = {
-        'range':f'{min_calories}-{max_calories}',
-        'options': resp.json()
+
+    if resp.status_code >= 400:
+        data = {
+            'error':'Try reducing your steps'
         }
+
+    else:
+        data = {
+            'number of meals requested': num_of_meals,
+            'range':f'per meal {min_calories}-{max_calories}',
+            'options': resp.json()
+            }
 
     return data
 
 
-def calc_calories(steps):
+def calc_calories(steps,num_of_meals):
     """Calculates and returns min and max calorie allotement"""
     min_calories_base = 500
     max_calories_base = 1000
@@ -36,5 +44,30 @@ def calc_calories(steps):
     burn_rate = .04
     additional_calories = int(steps * burn_rate)
 
-    return min_calories_base + additional_calories, max_calories_base + additional_calories
+
+
+    return (min_calories_base + additional_calories)/num_of_meals, (max_calories_base + additional_calories)/num_of_meals
+
+    """
+    Test # 1example if steps 5000, and num_of meals 1 = expected result
+         results = calc_calories(5000,1)
+         test_result = assert(expected_results,results)
+
+         if test_result:
+            pass test
+        else:
+            fail test
+
+    Test # 2 example if steps 5000, and num_of meals 1 = unexpected result
+
+         results = calc_calories(5000,1)
+         test_result = assertfail(expected_results,results)
+
+         if test_result:
+            pass test
+        else:
+            fail test
+    """
+
+
 
--- Pedometer-Activity-Meal-Finder-Main/app/take_home/views.py
+++ Pedometer-Activity-Meal-Finder-Main/app/take_home/views.py
@@ -4,16 +4,20 @@ from .funcs import get_dinner_options
 
 def dinner_options(request):
     """Provide one or two dinner options, more steps equal higher calorie meal options"""
+    try:
 
-    steps = request.GET.get('steps','0')
-    
-    #validate steps 
-    if steps.isdigit():
+        steps = request.GET.get('steps','0')
+        meals = request.GET.get('num_of_meals','1')
+        
+        #validate steps 
+        if steps.isdigit() and meals.isdigit():
 
-        dinner_data = get_dinner_options(int(steps))
-        return JsonResponse(dinner_data)
-    
-    else:
-        return JsonResponse({"error":"please provide steps in number form ex.123"})
+            dinner_data = get_dinner_options(int(steps),int(meals))
+            return JsonResponse(dinner_data)
+        
+        else:
+            return JsonResponse({"error":"please provide steps and meals in number form ex.123"})
+    except:
+            return JsonResponse({"400":"bad request"})
 
 
